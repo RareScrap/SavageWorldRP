7dc0a8b6785797ceb4bafff34faceaaa392359f7
RareScrap <RareScrap@users.noreply.github.com>
2017-12-25 20:45:23 +0800

    Рефакторинг GUI + новая текстура к прошлому коммиту

--- src/main/java/rsstats/client/gui/MainMenuGUI.java
+++ src/main/java/rsstats/client/gui/MainMenuGUI.java
@@ -1,25 +1,22 @@
 package rsstats.client.gui;
 
-import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.InventoryEffectRenderer;
 import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.inventory.Slot;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL12;
 import rsstats.common.CommonProxy;
+import rsstats.common.RSStats;
 import rsstats.common.network.PacketShowSkillsByStat;
 import rsstats.data.ExtendedPlayer;
 import rsstats.inventory.container.MainContainer;
 import rsstats.items.SkillItem;
-
-import java.util.List;
 
 /**
  * GUI для основного окна мода, содержащее информацию о персонаже (имя, уровень, здоровье, защита, харизма,
@@ -29,16 +26,11 @@ import java.util.List;
 public class MainMenuGUI extends InventoryEffectRenderer {
     /** Расположение фона GUI */
     private static final ResourceLocation background =
-            new ResourceLocation("rsstats","textures/gui/StatsAndInvTab_FIT.png");
-
-    /** Длина GUI в пикселях. Defined as  float, passed as int. */
-    private float xSizeFloat;
-    /** Ширина GUI в пикселях. Defined as  float, passed as int. */
-    private float ySizeFloat;
+            new ResourceLocation(RSStats.MODID,"textures/gui/StatsAndInvTab_FIT.png");
 
     public ExtendedPlayer player;
     /** UnlocalozedName текущей выбранно статы */
-    private String current="";
+    private String currentStat = "";
 
     /** Инвентарь для статов */
     // Could use IInventory type to be more generic, but this way will save an import...
@@ -54,19 +46,9 @@ public class MainMenuGUI extends InventoryEffectRenderer {
         // Высталяем размеры контейнера. Соответствует размерам GUI на текстуре.
         this.xSize = 340;
         this.ySize = 211;
-    }
-
-    /**
-     * Draws the screen and all the components in it
-     * @param mouseX
-     * @param mouseY
-     * @param partialTicks
-     */
-    @Override
-    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
-        super.drawScreen(mouseX, mouseY, partialTicks);
-        this.xSizeFloat = (float) mouseX;
-        this.ySizeFloat = (float) mouseY;
+        // Выставляем края контейнера (верхний и левый)
+        this.guiLeft = this.width/2 - xSize/2;
+        this.guiTop = this.height/2 - ySize/2;
     }
 
     /**
@@ -106,19 +88,10 @@ public class MainMenuGUI extends InventoryEffectRenderer {
         //GL11.glScalef(2.0F, 2.0F, 1.0F);
         this.mc.getTextureManager().bindTexture(background);
 
-        int k = this.width/2 - xSize/2;
-        int l = this.height/2 - ySize/2;
-
-        drawTexturedRect(k, l, 0, 0, xSize, ySize, xSize, ySize);
-
+        // Отрисовываем текстуру GUI
+        drawTexturedRect(this.guiLeft, this.guiTop, 0, 0, xSize, ySize, xSize, ySize);
         // Орисовываем превью игрока
-        drawPlayerModel(k+30, l+90, /*17*/ 40, (float)(k + 51) - this.xSizeFloat, (float)(l + 75 - 50) - this.ySizeFloat, this.mc.thePlayer);
-
-        int textY = l+113;
-        mc.fontRenderer.drawString("Шаг: " + player.getStep(), k+8, textY+=10, 0x444444, false);
-        mc.fontRenderer.drawString("Защита: " + player.getProtection(), k+8, textY+=10, 0x444444, false);
-        mc.fontRenderer.drawString("Стойкость: " + player.getPersistence(), k+8, textY+=10, 0x444444, false);
-        mc.fontRenderer.drawString("Харизма: " + player.getCharisma(), k+8, textY+=10, 0x444444, false);
+        drawPlayerModel(this.guiLeft+30, this.guiTop+90, /*17*/ 40, (float)(this.guiLeft + 51) - mouseX, (float)(this.guiTop + 75 - 50) - mouseY, this.mc.thePlayer);
 
         // Это было в туторах, но я хз на что это влияет. Слоты и рендер предметов работают и без этого
         /*for (int i1 = 0; i1 < this.inventorySlots.inventorySlots.size(); ++i1)
@@ -132,6 +105,26 @@ public class MainMenuGUI extends InventoryEffectRenderer {
     }
 
     /**
+     * Draw the foreground layer for the GuiContainer (everything in front of the items)
+     *
+     * @param p_146979_1_
+     * @param p_146979_2_
+     */
+    @Override
+    protected void drawGuiContainerForegroundLayer(int p_146979_1_, int p_146979_2_) {
+        int textY = 123;
+        mc.fontRenderer.drawString("Шаг: " + player.getStep(), 8, textY, 0x444444, false);
+        mc.fontRenderer.drawString("Уровень: " + player.getLvl(), 60, textY, 0x444444, false);
+        mc.fontRenderer.drawString("Защита: " + player.getProtection(), 8, textY+=10, 0x444444, false);
+        mc.fontRenderer.drawString("Очки опыта: " + player.getExp(), 60, textY, 0x444444, false);
+        mc.fontRenderer.drawString("Стойкость: " + player.getPersistence(), 8, textY+=10, 0x444444, false);
+        mc.fontRenderer.drawString("Усталость: " + player.getTiredness(), 60, textY, 0x444444, false);
+        mc.fontRenderer.drawString("Харизма: " + player.getCharisma(), 8, textY+=10, 0x444444, false);
+
+        super.drawGuiContainerForegroundLayer(p_146979_1_, p_146979_2_);
+    }
+
+    /**
      * Отрисовывает превью игрока
      * @param x TODO
      * @param y TODO
@@ -140,7 +133,7 @@ public class MainMenuGUI extends InventoryEffectRenderer {
      * @param pitch TODO
      * @param playerdrawn TODO
      */
-    public static void drawPlayerModel(int x, int y, int scale, float yaw, float pitch, EntityLivingBase playerdrawn) {
+    private static void drawPlayerModel(int x, int y, int scale, float yaw, float pitch, EntityLivingBase playerdrawn) {
         GL11.glEnable(GL11.GL_COLOR_MATERIAL);
         GL11.glPushMatrix();
         GL11.glTranslatef((float)x, (float)y, 50.0F);
@@ -177,38 +170,15 @@ public class MainMenuGUI extends InventoryEffectRenderer {
     }
 
     @Override
-    protected void mouseClicked(int mouseX, int mouseY, int mouseButton) {
-        //System.out.println("mouseClicked: " + mouseX + " " + mouseY + " " + mouseButton);
-        super.mouseClicked(mouseX, mouseY, mouseButton);
-    }
-
-    @Override
-    protected void mouseMovedOrUp(int p_146286_1_, int p_146286_2_, int p_146286_3_) {
-        //System.out.println("mouseMovedOrUp: " + p_146286_1_ + " " + p_146286_2_ + " " + p_146286_3_);
-        super.mouseMovedOrUp(p_146286_1_, p_146286_2_, p_146286_3_);
-    }
-
-    @Override
-    protected void handleMouseClick(Slot p_146984_1_, int p_146984_2_, int p_146984_3_, int p_146984_4_) {
-        //System.out.println("handleMouseClick: " + p_146984_1_ + " " + p_146984_2_ + " " + p_146984_3_ + " " + p_146984_4_);
-        super.handleMouseClick(p_146984_1_, p_146984_2_, p_146984_3_, p_146984_4_);
-    }
-
-    @Override
     protected void renderToolTip(ItemStack itemStack, int p_146285_2_, int p_146285_3_) {
         Item item = itemStack.getItem();
-        if (!item.getUnlocalizedName().equals(current) && !(item instanceof SkillItem)) {
+        if (!item.getUnlocalizedName().equals(currentStat) && !(item instanceof SkillItem)) {
             PacketShowSkillsByStat packet = new PacketShowSkillsByStat(itemStack.getItem().getUnlocalizedName());
             CommonProxy.INSTANCE.sendToServer(packet);
-            current = itemStack.getItem().getUnlocalizedName();
+            currentStat = itemStack.getItem().getUnlocalizedName();
         }
 
         super.renderToolTip(itemStack, p_146285_2_, p_146285_3_);
-    }
-
-    @Override
-    protected void drawHoveringText(List p_146283_1_, int p_146283_2_, int p_146283_3_, FontRenderer font) {
-        super.drawHoveringText(p_146283_1_, p_146283_2_, p_146283_3_, font);
     }
 
     /**
