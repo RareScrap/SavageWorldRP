3397f286703c553c6fad2c3bdfd88eb51c5557d7
RareScrap <RareScrap@users.noreply.github.com>
2018-05-02 14:19:27 +0800

    Верстак улучшений теперь сохраняет свое направление в NBT сущности. На форуме говорят, что это не оч - нужно юзать метадату http://www.minecraftforge.net/forum/topic/25239-solved1710-saving-block-direction-state/ http://cazzar.net/tutorials/minecraft/Tile-Entity-Updates-The-Quick-and-Dirty-Method/

--- src/main/java/rsstats/inventory/container/rsstats/blocks/UpgradeStationEntity.java
+++ src/main/java/rsstats/inventory/container/rsstats/blocks/UpgradeStationEntity.java
@@ -1,6 +1,9 @@
 package rsstats.inventory.container.rsstats.blocks;
 
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import rsstats.inventory.UpgradeStationInventory;
 
@@ -17,15 +20,38 @@ public class UpgradeStationEntity extends TileEntity {
         this.upgradeStationInventory = new UpgradeStationInventory();
     }
 
+    // TODO: Сохранять направление в NBT - нерационально. Нужно юзать метадату - http://www.minecraftforge.net/forum/topic/25239-solved1710-saving-block-direction-state/
     @Override
     public void writeToNBT(NBTTagCompound compound) {
         super.writeToNBT(compound);
         upgradeStationInventory.writeToNBT(compound);
+        compound.setInteger("dir", this.blockMetadata);
     }
 
     @Override
     public void readFromNBT(NBTTagCompound compound) {
         super.readFromNBT(compound);
         upgradeStationInventory.readFromNBT(compound);
+        this.blockMetadata = compound.getInteger("dir");
+
+    }
+
+    // Синхронизирует переменные блока с блоком на клиенте
+    @Override
+    public Packet getDescriptionPacket() {
+        NBTTagCompound tag = new NBTTagCompound();
+        writeToNBT(tag);
+        return new S35PacketUpdateTileEntity(xCoord, yCoord, zCoord, 0, tag);
+    }
+
+    // Нужно для метода выше
+    @Override
+    public void onDataPacket(NetworkManager net, S35PacketUpdateTileEntity pkt) {
+        readFromNBT(pkt.func_148857_g());
+    }
+
+    // http://cazzar.net/tutorials/minecraft/Tile-Entity-Updates-The-Quick-and-Dirty-Method/
+    public void markForUpdate() {
+        worldObj.markBlockForUpdate(xCoord, yCoord, zCoord);
     }
 }
